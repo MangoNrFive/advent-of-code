3ccf2a5140ff884d6fada0f0d511845730efb20f
MangoNrFive <p.wiedemer@gmail.com>
2023-12-07 23:01:38 +0100

    remove prints

--- 2023/07/02.py
+++ 2023/07/02.py
@@ -10,24 +10,17 @@ def get_type(hand):
 	counts = counters.values()
 
 	if len(counts) <= 1:
-		print("Five of a kind")
 		return 6  # Five of a kind
 	elif len(counts) == 2:
 		if max(counts) + jokers == 4:
-			print("Four of a kind")
 			return 5  # Four of a kind
-		print("Full House")
 		return 4  # Full House
 	elif len(counts) == 3:
 		if max(counts) + jokers == 3:
-			print("Three of a kind")
 			return 3  # Three of a kind
-		print("Two pair")
 		return 2  # Two pair
 	elif len(counts) == 4:
-		print("One pair")
 		return 1  # One pair
-	print("High card")
 	return 0  # High card
 
 
@@ -39,11 +32,9 @@ bids = []
 types = []
 for line in lines:
 	h, b = line.split()
-	print("-"*10)
-	print(h)
 	hands.append(h.replace("A", "E").replace("K", "D").replace("Q", "C").replace("J", "1").replace("T", "A"))
 	bids.append(int(b))
 	types.append(get_type(h))
 
 res = sum((rank0+1)*bid for rank0, (_, _, bid) in enumerate(sorted(zip(types, hands, bids))))
-print(res)  # 249103320
+print(res)
